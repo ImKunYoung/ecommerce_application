93870c520104f389ac6c3abea9d4ecc708190c8b
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-08 15:00:15 +0900

    GET http://localhost:8080/health_check

---
+++ ms-userservice/src/main/java/com/example/msuserservice/controller/UsersController.java
@@ -0,0 +1,16 @@
+package com.example.msuserservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UsersController {
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+}
