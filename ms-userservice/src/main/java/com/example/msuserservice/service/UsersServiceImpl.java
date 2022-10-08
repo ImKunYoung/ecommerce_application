fd99a54672f158e54fba5723d3657a0289ee5c5d
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-08 15:49:56 +0900

    dto to entity, save

--- ms-userservice/src/main/java/com/example/msuserservice/service/UsersServiceImpl.java
+++ ms-userservice/src/main/java/com/example/msuserservice/service/UsersServiceImpl.java
@@ -1,14 +1,31 @@
 package com.example.msuserservice.service;
 
 import com.example.msuserservice.dto.UserDto;
+import com.example.msuserservice.entity.UserEntity;
+import com.example.msuserservice.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_password");
+        userRepository.save(userEntity);
+
         return null;
     }
 
