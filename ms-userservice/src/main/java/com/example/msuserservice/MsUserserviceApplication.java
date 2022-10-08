5406e6bc4245f02055b5542cf053ede1a32d6ef0
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-08 14:51:47 +0900

    userservice 추가

---
+++ ms-userservice/src/main/java/com/example/msuserservice/MsUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUserserviceApplication.class, args);
+    }
+
+}
