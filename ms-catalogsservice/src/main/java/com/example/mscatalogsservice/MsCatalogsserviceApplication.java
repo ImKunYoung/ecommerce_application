454bfae97066aa7c6043a70d27a7c6baad1d3ac9
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 00:19:28 +0900

    init catalogsservice

---
+++ ms-catalogsservice/src/main/java/com/example/mscatalogsservice/MsCatalogsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscatalogsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogsserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCatalogsserviceApplication.class, args);
+    }
+
+}
