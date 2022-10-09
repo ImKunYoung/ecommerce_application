61e4d22be4e4222b5ff39db9cc6d63a73b149884
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 00:30:55 +0900

    유레카 클라이언트 등록 허용

--- ms-catalogsservice/src/main/java/com/example/mscatalogsservice/MsCatalogsserviceApplication.java
+++ ms-catalogsservice/src/main/java/com/example/mscatalogsservice/MsCatalogsserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.mscatalogsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsCatalogsserviceApplication {
 
     public static void main(String[] args) {
