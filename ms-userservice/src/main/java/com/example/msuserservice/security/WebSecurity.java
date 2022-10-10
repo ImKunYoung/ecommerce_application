42758b9765630f00a7626b29101156768f2f16f7
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 21:33:08 +0900

    Security에 필터 추가

--- ms-userservice/src/main/java/com/example/msuserservice/security/WebSecurity.java
+++ ms-userservice/src/main/java/com/example/msuserservice/security/WebSecurity.java
@@ -5,16 +5,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+
+
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .hasIpAddress("192.168.0.0") /*TODO: Change to Gateway IP*/
+                .and()
+                .addFilter(getAuthenticationFilter());
+
         http.headers().frameOptions().disable();
+    }
+
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 
 }
