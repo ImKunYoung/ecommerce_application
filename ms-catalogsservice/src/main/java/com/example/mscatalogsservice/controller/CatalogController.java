1f26c887cb924bed98c189a250cb29e3fc75b2bf
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 14:56:48 +0900

    health_check api 추기

--- ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
+++ ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
@@ -3,8 +3,11 @@ package com.example.mscatalogsservice.controller;
 import com.example.mscatalogsservice.service.CatalogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/ms-catalogsservice")
@@ -16,6 +19,12 @@ public class CatalogController {
     private final Environment env;
 
     private final CatalogService catalogService;
+
+
+    @GetMapping("/health_check")
+    public String status(HttpServletRequest request) {
+        return String.format("It's Working in Catalog Service on PORT %s", request.getServerPort());
+    }
 
 
 
