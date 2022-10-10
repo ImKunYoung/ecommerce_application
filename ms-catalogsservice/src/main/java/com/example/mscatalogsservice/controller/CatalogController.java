d72e1100463b3f04827a2ed8a0d9b58641cfbd1d
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 14:52:30 +0900

    카탈로그 컨트롤러 추가

--- ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
+++ ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
@@ -1,5 +1,23 @@
 package com.example.mscatalogsservice.controller;
 
+import com.example.mscatalogsservice.service.CatalogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ms-catalogsservice")
+@RequiredArgsConstructor
 public class CatalogController {
     /*TODO: 32, r01-7*/
+
+
+    private final Environment env;
+
+    private final CatalogService catalogService;
+
+
+
+
 }
