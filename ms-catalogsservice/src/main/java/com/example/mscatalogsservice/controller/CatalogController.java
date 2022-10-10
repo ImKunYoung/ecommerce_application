3d4e268a6bad26995e26229fa403599beb5566e0
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 15:02:07 +0900

    모든 카탈로그 확인하기 api 추가

--- ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
+++ ms-catalogsservice/src/main/java/com/example/mscatalogsservice/controller/CatalogController.java
@@ -1,24 +1,40 @@
 package com.example.mscatalogsservice.controller;
 
+import com.example.mscatalogsservice.entity.CatalogEntity;
 import com.example.mscatalogsservice.service.CatalogService;
+import com.example.mscatalogsservice.vo.ResponseCatalog;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ms-catalogsservice")
 @RequiredArgsConstructor
 public class CatalogController {
-    /*TODO: 32, r01-7*/
 
 
     private final Environment env;
 
     private final CatalogService catalogService;
+
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+        Iterable<CatalogEntity> allCatalogs = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        allCatalogs.forEach(v -> result.add(new ModelMapper().map(v, ResponseCatalog.class)));
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 
 
     @GetMapping("/health_check")
