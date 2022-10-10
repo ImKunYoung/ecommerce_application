066ed9e9cb0ac76ccd40d8e66bd58a0a310625f4
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 15:51:44 +0900

    리포지토리 테스트 코드

---
+++ ms-catalogsservice/src/test/java/com/example/mscatalogsservice/repository/CatalogRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.mscatalogsservice.repository;
+
+import com.example.mscatalogsservice.entity.CatalogEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class CatalogRepositoryTest {
+
+
+    @Autowired
+    private CatalogRepository catalogRepository;
+
+
+
+    @Test
+    public void 게시글저장_불러오기() {
+
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        CatalogEntity catalogEntity = CatalogEntity.builder()
+                .productId("productId")
+                .productName("productName")
+                .stock(1)
+                .unitPrice(1)
+                .build();
+
+        catalogRepository.save(catalogEntity);
+
+
+        catalogRepository.findById(catalogEntity.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + catalogEntity.getId()));
+
+
+    }
+
+
+
+}
