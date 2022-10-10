297355be0f1b9088af83aa102fbe508a8e327134
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 16:37:41 +0900

    작동 상태 확인 api 추가

--- ms-ordersservice/src/main/java/com/example/msordersservice/controller/OrderController.java
+++ ms-ordersservice/src/main/java/com/example/msordersservice/controller/OrderController.java
@@ -1,10 +1,18 @@
 package com.example.msordersservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class OrderController {
 
 
+    /*@Description 작동 상태 확인*/
+    @GetMapping("/health_check")
+    public String status(HttpServletRequest request) {
+        return String.format("It's Working in Order Service on PORT %s", request.getServerPort());
+    }
 
 }
