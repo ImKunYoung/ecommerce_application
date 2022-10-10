27654e3a2a05318f5e3d66cc4c608f543685ea4f
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 16:19:44 +0900

    OrderEntity 추가

---
+++ ms-ordersservice/src/main/java/com/example/msordersservice/entity/OrderEntity.java
@@ -0,0 +1,45 @@
+package com.example.msordersservice.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@Table(name = "orders")
+public class OrderEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String productId;
+
+    @Column(nullable = false)
+    private Integer qty;
+
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    private Date createdAt;
+
+}
