40dc01724b621b2843f4f1ae522fce477ab727ac
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 16:07:26 +0900

    init ms-ordersservice

---
+++ ms-ordersservice/src/main/java/com/example/msordersservice/MsOrdersserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msordersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrdersserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrdersserviceApplication.class, args);
+    }
+
+}
