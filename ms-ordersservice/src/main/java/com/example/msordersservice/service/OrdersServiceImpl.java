dd463dc62398b3186bf5146002b17ac25f27d637
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-10 16:27:48 +0900

    OrdersServiceImpl 추가

---
+++ ms-ordersservice/src/main/java/com/example/msordersservice/service/OrdersServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.msordersservice.service;
+
+import com.example.msordersservice.dto.OrderDto;
+
+public class OrdersServiceImpl implements OrdersService{
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDetails) {
+        return null;
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        return null;
+    }
+
+    @Override
+    public Iterable<OrderDto> getOrdersByUserId(String userId) {
+        return null;
+    }
+
+}
