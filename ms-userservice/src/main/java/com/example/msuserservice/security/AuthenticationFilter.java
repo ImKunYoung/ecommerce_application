c524c34431472b3459b2bfb556ab9b72be4c7837
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-11 11:30:02 +0900

    인증 성공 시 사용자에게 토큰 발행

--- ms-userservice/src/main/java/com/example/msuserservice/security/AuthenticationFilter.java
+++ ms-userservice/src/main/java/com/example/msuserservice/security/AuthenticationFilter.java
@@ -1,11 +1,15 @@
 package com.example.msuserservice.security;
 
+import com.example.msuserservice.service.UsersService;
 import com.example.msuserservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.userdetails.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +18,11 @@ import javax.servlet.FilterChain;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@RequiredArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final UsersService usersService;
+    private final Environment env;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -34,6 +42,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String userName = ((User) authResult.getPrincipal()).getUsername();
         super.successfulAuthentication(request, response, chain, authResult);
     }
 
