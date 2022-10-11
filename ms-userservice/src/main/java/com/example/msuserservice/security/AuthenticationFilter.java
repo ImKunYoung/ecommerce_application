16cc2f8a26b44a5fa449f6c1fd45ea35ae0b0b22
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-11 11:33:16 +0900

    User Service 사용할 수 있도록 메소드 수정

--- ms-userservice/src/main/java/com/example/msuserservice/security/AuthenticationFilter.java
+++ ms-userservice/src/main/java/com/example/msuserservice/security/AuthenticationFilter.java
@@ -3,8 +3,8 @@ package com.example.msuserservice.security;
 import com.example.msuserservice.service.UsersService;
 import com.example.msuserservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -18,11 +18,13 @@ import javax.servlet.FilterChain;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@RequiredArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final UsersService usersService;
-    private final Environment env;
+    private UsersService usersService;
+    private Environment env;
+
+    public AuthenticationFilter(UsersService usersService, Environment env, AuthenticationManager authenticationManager) {
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -43,6 +45,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         String userName = ((User) authResult.getPrincipal()).getUsername();
+        /*TODO: Refactor*/
         super.successfulAuthentication(request, response, chain, authResult);
     }
 
