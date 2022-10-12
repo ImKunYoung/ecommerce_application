cf0c8808c5cb7b52a9faf7a7bb35dd295f343be0
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-12 16:49:03 +0900

    init Config Server

---
+++ msa-configservice/src/main/java/com/example/msaconfigservice/MsaConfigserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaConfigserviceApplication.class, args);
+    }
+
+}
---
+++ msa-configservice/src/test/java/com/example/msaconfigservice/MsaConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
