2e1eefab286f4ce0ddb1c0ea1601ece19975126d
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-12 16:49:29 +0900

    EnableConfigServer 설정

--- msa-configservice/src/test/java/com/example/msaconfigservice/MsaConfigserviceApplicationTests.java
+++ msa-configservice/src/test/java/com/example/msaconfigservice/MsaConfigserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.msaconfigservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootTest
+@EnableConfigServer
 class MsaConfigserviceApplicationTests {
 
     @Test
