758d8cc39aad040253a1d49ff2e5ea46590859d8
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-15 14:01:29 +0900

    #ms-userservice 로드 밸런싱을 위한 준비

--- ms-userservice/src/main/java/com/example/msuserservice/MsUserserviceApplication.java
+++ ms-userservice/src/main/java/com/example/msuserservice/MsUserserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.msuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class MsUserserviceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
