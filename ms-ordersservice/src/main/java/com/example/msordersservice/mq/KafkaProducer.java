b290f207b3040df399404ead87f5597d0febd94e
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-28 13:22:54 +0900

    #ms-ordersservice 서비스

---
+++ ms-ordersservice/src/main/java/com/example/msordersservice/mq/KafkaProducer.java
@@ -0,0 +1,29 @@
+package com.example.msordersservice.mq;
+
+import com.example.msordersservice.dto.OrderDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public OrderDto send(String kafkaTopic, OrderDto orderDto) {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "";
+        try {
+            jsonInString = mapper.writeValueAsString(orderDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        kafkaTemplate.send(kafkaTopic, jsonInString);
+        log.info("Kafka Producer send data from the ms-ordersservice: " + orderDto);
+
+        return orderDto;
+    }
+}
