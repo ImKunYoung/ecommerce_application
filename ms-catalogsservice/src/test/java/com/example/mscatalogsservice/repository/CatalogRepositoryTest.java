d8429f7c4e3f923644d3935ae268b5795072b7e5
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-30 10:41:38 +0900

    ref: Update source code quality
    Signed-off-by: ImKunYoung <ur2kunyoung2@outlook.com>

--- ms-catalogsservice/src/test/java/com/example/mscatalogsservice/repository/CatalogRepositoryTest.java
+++ ms-catalogsservice/src/test/java/com/example/mscatalogsservice/repository/CatalogRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class CatalogRepositoryTest {
+class CatalogRepositoryTest {
 
 
     @Autowired
@@ -16,7 +16,7 @@ public class CatalogRepositoryTest {
 
 
     @Test
-    public void 게시글저장_불러오기() {
+    void 게시글저장_불러오기() {
 
         // given
         String title = "테스트 게시글";
